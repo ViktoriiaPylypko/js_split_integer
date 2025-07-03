4a9d69345ea87fdc4635d403aaba9c754d4a6b7f
Viktoriia Pylypko <pylpkoviktoria@gmail.com>
2025-07-03 19:45:39 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -2,7 +2,7 @@
 
 const splitInteger = require('./splitInteger');
 
-test(`should split a number into equal parts
+test(`should split number into equal parts
   if a value is divisible by a numberOfParts`, () => {
   expect(splitInteger(12, 2)).toEqual([6, 6]);
 });
